798bdb413b6a28903ad302a9a65faf7efe928360
agomez99 <austine.gomez@yahoo.com>
2020-06-15 22:03:41 -0500

    test

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+
+
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
@@ -17,6 +19,9 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI );
+
+
+
 
 // Start the API server
 app.listen(PORT, function() {
