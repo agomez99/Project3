33258ccb694d93ce1ad37ba655d39d109714dec2
agomez99 <austine.gomez@yahoo.com>
2020-06-16 12:22:37 -0500

    added hashed passwords with bcrypt and JWT

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
-    email:{type: String, required: true},
+    email:{type: String, required: true, unique:true},
     password: {type: String, required: true, minlength: 5},
     displayName:{type: String},
 })
-module.export = User = mongoose.model("user", userSchema);
+module.exports = User = mongoose.model("user", userSchema);
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.listen(PORT, function() {
 });
 
 //mongoose
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology:true,},
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology:true,useCreateIndex: true},
   (err)=> {
     if(err) throw err;
     console.log("Mongoose connection established")
