d858110b9987736050b2ae70479c3c8e0f0d376a
agomez99 <austine.gomez@yahoo.com>
2020-06-16 00:47:12 -0500

    added server setup with mongoDB

--- server.js
+++ server.js
@@ -1,29 +1,28 @@
 const express = require("express");
-
-
-
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const cors = require('cors');
+require("dotenv").config();
+
+//express
 const app = express();
-const PORT = process.env.PORT || 3001;
-
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-// Add routes, both API and view
-app.use(routes);
-
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI );
+app.use(cors());
 
 
+const PORT = process.env.PORT ||5000;
 
 
 // Start the API server
 app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  console.log(`🌎  ==> API Server now listening on PORT${PORT}!`);
 });
+
+//mongoose
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology:true,},
+  (err)=> {
+    if(err) throw err;
+    console.log("Mongoose connection established")
+  });
+
+  //routes
+  app.use("/users", require("./routes/userRouter"));
