b0dc67b89124cfdbf376622a61a3eaa644273846
Jiovanie Martinez <jiovaniemartinez@gmail.com>
2020-06-17 11:58:39 -0500

    linked all pages with the router

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-import MainNavbar from './Components/Navbar/Navbar';
-import Title from './Components/Title/Title';
-
-
+import LoginPage from "./Pages/LoginPage";
+import ProfilePage from "./Pages/ProfilePage";
+import BlogsPage from "./Pages/BlogsPage";
+import TutorialPage from './Pages/TutorialPage';
+import UserStoryPage from './Pages/UserStoryPage';
 
 function App() {
   return (
-    <div className="App">
-      <MainNavbar />
-      <Title />
-     
-    </div>
+    <Router>
+      <div className="App">
+
+        <Switch>
+          <Route exact path="/" component={LoginPage} />
+          <Route exact path="/profile" component={ProfilePage} />
+          <Route exact path="/blog" component={BlogsPage} />
+          <Route exact path="/tutorial-page" component={TutorialPage} />
+          <Route exact path="/user-story-page" component={UserStoryPage} />
+        </Switch>
+
+      </div>
+    </Router>
+
   );
 }
 
