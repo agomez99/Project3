370dadb91d9d7d7cdc91ab9a6d5d8adfa6ac0ae9
Austine Gomez <austine.gomez@yahoo.com>
2020-06-18 00:55:23 -0500

    added files

--- server.js
+++ server.js
@@ -1,28 +1,24 @@
 const express = require("express");
+
 const mongoose = require("mongoose");
-const cors = require('cors');
-require("dotenv").config();
-
-//express
+const routes = require("./routes");
 const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Add routes, both API and view
+app.use(routes);
 
-
-const PORT = process.env.PORT||5000;
-
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
 // Start the API server
 app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}  `);
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
-
-//mongoose
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology:true,useCreateIndex: true},
-  (err)=> {
-    if(err) throw err;
-    console.log("Mongoose connection established")
-  });
-
-  //routes
-  app.use("/users", require("./routes/userRouter"));
