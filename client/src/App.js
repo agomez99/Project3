e9de8eddc35af2caffb0da660d8596d35fe6c72b
Jiovanie Martinez <jiovaniemartinez@gmail.com>
2020-06-19 16:06:04 -0500

    got flexbox working for the footer

--- client/src/App.js
+++ client/src/App.js
@@ -8,23 +8,26 @@ import TutorialPage from './Pages/TutorialPage';
 import UserStoryPage from './Pages/UserStoryPage';
 import NewUserPage from './Pages/NewUserPage';
 import MainNavbar from './Components/Navbar/Navbar';
+import MainFooter from './Components/Footer/Footer';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <MainNavbar />
-        <Switch>
-          <Route exact path="/" component={LoginPage} />
-          <Route exact path="/new-user" component={NewUserPage} />
-          <Route exact path="/profile" component={ProfilePage} />
-          <Route exact path="/blog" component={BlogsPage} />
-          <Route exact path="/tutorial-page" component={TutorialPage} />
-          <Route exact path="/user-story-page" component={UserStoryPage} />
-        </Switch>
-
+    <div className='page-container'>
+      <div className='content-wrap'>
+        <Router>
+          <MainNavbar />
+          <Switch>
+            <Route exact path="/" component={LoginPage} />
+            <Route exact path="/new-user" component={NewUserPage} />
+            <Route exact path="/profile" component={ProfilePage} />
+            <Route exact path="/blog" component={BlogsPage} />
+            <Route exact path="/tutorial-page" component={TutorialPage} />
+            <Route exact path="/user-story-page" component={UserStoryPage} />
+          </Switch>
+          <MainFooter />
+        </Router>
       </div>
-    </Router>
+    </div>
 
   );
 }
