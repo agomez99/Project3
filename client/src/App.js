e9bb6de3ac7b1c5f50178cf5ac6ee7f499e86926
Jiovanie Martinez <jiovaniemartinez@gmail.com>
2020-06-19 23:14:42 -0500

    got a handle on the input information for form

--- client/src/App.js
+++ client/src/App.js
@@ -12,23 +12,24 @@ import MainFooter from './Components/Footer/Footer';
 
 function App() {
   return (
-    <div className='page-container'>
-      <div className='content-wrap'>
-        <Router>
-          <MainNavbar />
-          <Switch>
-            <Route exact path="/" component={LoginPage} />
-            <Route exact path="/new-user" component={NewUserPage} />
-            <Route exact path="/profile" component={ProfilePage} />
-            <Route exact path="/blog" component={BlogsPage} />
-            <Route exact path="/tutorial-page" component={TutorialPage} />
-            <Route exact path="/user-story-page" component={UserStoryPage} />
-          </Switch>
-          <MainFooter />
-        </Router>
+    <div className='App'>
+      <div className='page-container'>
+        <div className='content-wrap'>
+          <Router>
+            <MainNavbar />
+            <Switch>
+              <Route exact path="/" component={LoginPage} />
+              <Route exact path="/new-user" component={NewUserPage} />
+              <Route exact path="/profile" component={ProfilePage} />
+              <Route exact path="/blog" component={BlogsPage} />
+              <Route exact path="/tutorial-page" component={TutorialPage} />
+              <Route exact path="/user-story-page" component={UserStoryPage} />
+            </Switch>
+            <MainFooter />
+          </Router>
+        </div>
       </div>
     </div>
-
   );
 }
 
