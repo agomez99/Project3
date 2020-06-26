f3e983cf47d8ab67cdd6b89055735768d8a554a3
agomez99 <austine.gomez@yahoo.com>
2020-06-26 09:45:50 -0500

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: `https://codessource.herokuapp.com/`
+  baseUrl = process.env.baseURL || "http://localhost:5000"
+
+  //baseURL: `https://codessource.herokuapp.com/`
+
 });
