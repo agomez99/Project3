51fb9a84dea66d82cafec6d5b9abd434b58a541f
agomez99 <austine.gomez@yahoo.com>
2020-07-01 21:36:41 -0500

    seperated comments

--- client/src/Components/Disqus/Disqus2.js
+++ client/src/Components/Disqus/Disqus2.js
@@ -1,20 +1,20 @@
 import React, { Component } from "react"
-import Disqus from "disqus-react"
+import Disqus2 from "disqus-react"
 
 
 export default class extends Component {
 
     
   render() {
-    const disqusShortname = "codesource-1"
+    const disqusShortname = "codesource-2"
     const disqusConfig = {
       url: "",
-      identifier: "",
+      identifier: "/nodejs-beginner",
       title: "Title of Your Article",
       language: "en"
     }
 
-
+    
     
     return (
       <div className="article-container">
