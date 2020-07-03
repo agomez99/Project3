34e2c6668f82518d13986d3faf014c3504654c0f
agomez99 <austine.gomez@yahoo.com>
2020-07-03 16:29:32 -0500

    post css

--- client/src/Components/Post/Post.js
+++ client/src/Components/Post/Post.js
@@ -1,38 +1,64 @@
-import React from 'react';
-import moment from 'moment';
-import { Link } from 'react-router-dom';
-import Disqus from '../Disqus/Disqus';
-import Chip from '@material-ui/core/Chip';
+import React from "react";
+import moment from "moment";
+import { Link } from "react-router-dom";
+import Disqus from "../Disqus/Disqus";
+import Chip from "@material-ui/core/Chip";
+import {
+  EmailShareButton,
+  EmailIcon,
+  FacebookShareButton,
+  FacebookIcon,
+  LinkedinShareButton,
+  RedditShareButton,
+  RedditIcon,
+  TwitterIcon,
+  LinkedinIcon,
+  TwitterShareButton,
+} from "react-share";
+import "./styles.css";
 
-import './styles.css'
-
-const Post = props => {
+const Post = (props) => {
   const data = props.location.state;
-  console.log(data.tags)
+  console.log(data);
   return (
     <div className="post">
       <h1 className="p-title">{data.title}</h1>
-      <div dangerouslySetInnerHTML={{__html: data.body}} />
-      <Chip 
-            label={data.tags[0].name}
-          />
-          <Chip 
-            label={data.tags[1].name}
-          />
-          <Chip 
-            label={data.tags[2].name}
-          />
+      <div dangerouslySetInnerHTML={{ __html: data.body }} />
+      <Chip label={data.tags[0].name} />
+      <Chip label={data.tags[1].name} />
+      <Chip label={data.tags[2].name} />
+      <container className="share-btn">
+        <label>Share</label>{""}
+      <EmailShareButton key={"email"} className={"btn"} url={data.url}>
+        <EmailIcon round size={32} />
+      </EmailShareButton>
+      <FacebookShareButton key={"fb"} className={"btn"} url={data.url}>
+        <FacebookIcon round size={32} />
+      </FacebookShareButton>
+      <LinkedinShareButton key={"linked"} className={"btn"} url={data.url}>
+        <LinkedinIcon rouLinkedinShareButtonnd size={32} />
+      </LinkedinShareButton>
+      <TwitterShareButton key={"twitter"} className={"btn"} url={data.url}>
+        <TwitterIcon round size={32} />
+      </TwitterShareButton>
+      <RedditShareButton key={"reddit"} className={"btn"} url={data.url}>
+        <RedditIcon round size={32} />
+      </RedditShareButton>
+      </container>
       <hr />
+
       <div className="author">
-        <img src={data.author.profile_image} alt="Author" className="author-image"/>
+        <img
+          src={data.author.profile_image}
+          alt="Author"
+          className="author-image"
+        />
         <div>
-          Published by
-          {' '}
+          Published by{" "}
           <strong>
             {data.author.first_name} {data.author.last_name}
           </strong>
-          <p>{data.author.linkedin_url}</p>
-          {' '} on {' '}
+          <p>{data.author.linkedin_url}</p> on{" "}
           {moment(data.published).format("MMMM Do, YYYY")}
         </div>
       </div>
@@ -40,11 +66,8 @@ const Post = props => {
       <hr />
       <Link to="/blog">&larr; Back to the posts list</Link>
       <Disqus />
-
-
-
     </div>
   );
-}
+};
 
 export default Post;
